6512162b73eb6e318c796d1d534e75aae3b1879c
Mike Walters <mike@eventbooking.com>
2016-01-07 16:48:24 -0500

    invalid fixes

--- demo/forms/forms.js
+++ demo/forms/forms.js
@@ -1,6 +1,9 @@
 /// <reference path="../app.js"/>
 
 function FormsController($routeParams) {
+    this.submit = function() {
+        
+    }
 }
 
 app.controller('formsController', FormsController);
