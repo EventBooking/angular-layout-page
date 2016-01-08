b9d835614224657fcac896d70151917e611a1316
Mike Walters <mike@eventbooking.com>
2016-01-08 12:09:40 -0500

    stylin

--- demo/app.js
+++ demo/app.js
@@ -1,60 +1,60 @@
 /* global angular */
 
 function Config($routeProvider, $locationProvider) {
-	$locationProvider.html5Mode({ enabled: true })
+    $locationProvider.html5Mode({ enabled: true })
 
-	$routeProvider
-		
+    $routeProvider
+
         .when("/forms", {
-			templateUrl: "/forms/forms.html",
-			controller: 'formsController',
-			controllerAs: 'vm'
-		})
+            templateUrl: "/forms/forms.html",
+            controller: 'formsController',
+            controllerAs: 'vm'
+        })
         .when("/buttons", {
-			templateUrl: "/buttons/buttons.html",
-			controller: 'buttonsController',
-			controllerAs: 'vm'
-		})
+            templateUrl: "/buttons/buttons.html",
+            controller: 'buttonsController',
+            controllerAs: 'vm'
+        })
         .when("/routetest/:area", {
-			templateUrl: "/paneTest/paneTest.html",
-			controller: 'paneTestController',
-			controllerAs: 'vm'
-		})
+            templateUrl: "/paneTest/paneTest.html",
+            controller: 'paneTestController',
+            controllerAs: 'vm'
+        })
         .when("/routetest", {
-			redirectTo: '/routetest/twoPane'
-		})
-		.when("/panes/:area", {
-			templateUrl: "/paneTest/paneTest.html",
-			controller: 'paneTestController',
-			controllerAs: 'vm'
-		})
+            redirectTo: '/routetest/twoPane'
+        })
+        .when("/panes/:area", {
+            templateUrl: "/paneTest/paneTest.html",
+            controller: 'paneTestController',
+            controllerAs: 'vm'
+        })
         .when("/panes", {
-			redirectTo: '/panes/twoPane'
-		})
-		.when("/pages/fullscreen", {
-			templateUrl: "/pageTest/pageTest.html",
-			controller: 'pageTestController',
-			controllerAs: 'vm'
-		})
+            redirectTo: '/panes/twoPane'
+        })
+        .when("/pages/fullscreen", {
+            templateUrl: "/pageTest/pageTest.html",
+            controller: 'pageTestController',
+            controllerAs: 'vm'
+        })
         .when("/text", {
-			templateUrl: "/text/text.html",
-			controller: 'textController',
-			controllerAs: 'vm'
-		})
-		.when("/pages", {
-			redirectTo: '/pages/fullscreen'
-		})
-		.otherwise({
-			redirectTo: '/panes'
-		});
+            templateUrl: "/text/text.html",
+            controller: 'textController',
+            controllerAs: 'vm'
+        })
+        .when("/pages", {
+            redirectTo: '/pages/fullscreen'
+        })
+        .otherwise({
+            redirectTo: '/panes'
+        });
 }
 
 Run.$inject = ['$rootScope'];
 
 function Run($rootScope) {
-	
+
 }
 
-angular.module("demo", ['ngRoute', 'ngAnimate', 'ngLayoutPage'])
-	.config(Config)
-	.run(Run);
+angular.module("demo", ['ngRoute', 'ngAnimate', 'ngLayoutPage', 'ngResource'])
+    .config(Config)
+    .run(Run);
