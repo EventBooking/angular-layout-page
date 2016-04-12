d8a82900d4b1433d18dffcd515bfaed11f31f1d3
Troy McNeil <troy@eventbooking.com>
2016-04-12 11:40:22 -0400

    Tabs

--- dist/vops-layout.templates.js
+++ dist/vops-layout.templates.js
@@ -74,9 +74,9 @@ try {
   module = angular.module('ngLayoutPage', []);
 }
 module.run(['$templateCache', function($templateCache) {
-  $templateCache.put('nav-group-item/nav-group-item.html',
-    '<i ng-if="vm.hasIcon" class="nav-group-item-icon" ng-class="vm.iconClass"></i>\n' +
-    '<span class="nav-group-item-text" ng-transclude></span>');
+  $templateCache.put('nav-header/nav-header.html',
+    '<span class="nav-header-text">{{vm.text}}</span>\n' +
+    '<span class="nav-header-small">{{vm.small}}</span>');
 }]);
 })();
 
@@ -87,9 +87,9 @@ try {
   module = angular.module('ngLayoutPage', []);
 }
 module.run(['$templateCache', function($templateCache) {
-  $templateCache.put('nav-header/nav-header.html',
-    '<span class="nav-header-text">{{vm.text}}</span>\n' +
-    '<span class="nav-header-small">{{vm.small}}</span>');
+  $templateCache.put('nav-group-item/nav-group-item.html',
+    '<i ng-if="vm.hasIcon" class="nav-group-item-icon" ng-class="vm.iconClass"></i>\n' +
+    '<span class="nav-group-item-text" ng-transclude></span>');
 }]);
 })();
 
