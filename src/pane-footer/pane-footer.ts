71485cb335032f14b25bfc013fb22d3239265a91
Mike Walters <mike@eventbooking.com>
2018-01-29 15:11:56 -0500

    fix for font-family when using normalize

--- src/pane-footer/pane-footer.ts
+++ src/pane-footer/pane-footer.ts
@@ -2,6 +2,8 @@
 
     class PaneFooterDirective {
         restrict = 'E';
+        transclude = true;
+        template = '<div class="pane-footer-content" ng-transclude></div>';
 
         link = ($scope, $element) => {
             $element.parent(".pane").addClass("pane--withFooter");
