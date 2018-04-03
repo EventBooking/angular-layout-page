c5f6b1e13b73ca663ac35717bf1fe8d8000ba40b
Mike Walters <mike@eventbooking.com>
2018-04-03 08:42:16 -0400

    https://github.com/EventBooking/VenueOps.Meetings/issues/1160 and https://github.com/EventBooking/VenueOps.Meetings/issues/1159

--- dist/vops-layout.templates.js
+++ dist/vops-layout.templates.js
@@ -5,6 +5,23 @@ try {
   module = angular.module('ngLayoutPage', []);
 }
 module.run(['$templateCache', function($templateCache) {
+  $templateCache.put('doughnut/doughnut.html',
+    '<div class="doughnut-text">\n' +
+    '    <div ng-transclude></div>\n' +
+    '</div>\n' +
+    '<canvas class="doughnut-hole"></canvas>\n' +
+    '<canvas class="doughnut-fill"></canvas>\n' +
+    '<canvas class="doughnut-bg"></canvas>');
+}]);
+})();
+
+(function(module) {
+try {
+  module = angular.module('ngLayoutPage');
+} catch (e) {
+  module = angular.module('ngLayoutPage', []);
+}
+module.run(['$templateCache', function($templateCache) {
   $templateCache.put('bar-graph/bar-graph.html',
     '<div class="bar-graph-bg">\n' +
     '    <div class="bar-graph-fill" ng-class="{\'bar-graph-fill--full\': vm.isFull}" ng-style="vm.style" ng-transclude></div>\n' +
@@ -60,45 +77,9 @@ try {
   module = angular.module('ngLayoutPage', []);
 }
 module.run(['$templateCache', function($templateCache) {
-  $templateCache.put('doughnut/doughnut.html',
-    '<div class="doughnut-text">\n' +
-    '    <div ng-transclude></div>\n' +
-    '</div>\n' +
-    '<canvas class="doughnut-hole"></canvas>\n' +
-    '<canvas class="doughnut-fill"></canvas>\n' +
-    '<canvas class="doughnut-bg"></canvas>');
-}]);
-})();
-
-(function(module) {
-try {
-  module = angular.module('ngLayoutPage');
-} catch (e) {
-  module = angular.module('ngLayoutPage', []);
-}
-module.run(['$templateCache', function($templateCache) {
   $templateCache.put('nav-group-item/nav-group-item.html',
     '<i ng-if="vm.hasIcon" class="nav-group-item-icon" ng-class="vm.iconClass"></i>\n' +
     '<span class="nav-group-item-text" ng-transclude></span>');
-}]);
-})();
-
-(function(module) {
-try {
-  module = angular.module('ngLayoutPage');
-} catch (e) {
-  module = angular.module('ngLayoutPage', []);
-}
-module.run(['$templateCache', function($templateCache) {
-  $templateCache.put('page-dropdown/page-dropdown.html',
-    '<div class="page-dropdown" ng-class="{\'page-dropdown--isVisible\': vm.showIf}">\n' +
-    '    <div class="page-dropdown-menu">\n' +
-    '        <div class="page-dropdown-content" ng-transclude></div>\n' +
-    '        <div class="page-dropdown-close" ng-click="vm.showIf=false">\n' +
-    '            <i class="fa fa-angle-up"></i>\n' +
-    '        </div>\n' +
-    '    </div>\n' +
-    '</div>');
 }]);
 })();
 
@@ -141,17 +122,13 @@ try {
   module = angular.module('ngLayoutPage', []);
 }
 module.run(['$templateCache', function($templateCache) {
-  $templateCache.put('pane-header/pane-header.html',
-    '<a href="#" class="pane-header-close" ng-if="vm.showClose" ng-click="vm.close()">\n' +
-    '    <span class="pane-header-closeIcon fa fa-close"></i>\n' +
-    '</a>\n' +
-    '<div class="pane-header-actions" ng-transclude></div>\n' +
-    '<div class="pane-header-titles">\n' +
-    '    <div class="pane-header-title">\n' +
-    '        <span class="pane-header-titleText">{{vm.title}}</span>\n' +
-    '    </div>\n' +
-    '    <div class="pane-header-subtitle">\n' +
-    '        <span class="pane-header-subtitleText" ng-if="vm.subtitle">{{vm.subtitle}}</span>\n' +
+  $templateCache.put('page-dropdown/page-dropdown.html',
+    '<div class="page-dropdown" ng-class="{\'page-dropdown--isVisible\': vm.showIf}">\n' +
+    '    <div class="page-dropdown-menu">\n' +
+    '        <div class="page-dropdown-content" ng-transclude></div>\n' +
+    '        <div class="page-dropdown-close" ng-click="vm.showIf=false">\n' +
+    '            <i class="fa fa-angle-up"></i>\n' +
+    '        </div>\n' +
     '    </div>\n' +
     '</div>');
 }]);
@@ -190,6 +167,29 @@ module.run(['$templateCache', function($templateCache) {
     '    <div class="tab-content-window" \n' +
     '        ng-style="{\'width\': vm.width, \'left\': vm.tabPosition}"\n' +
     '        ng-transclude>\n' +
+    '    </div>\n' +
+    '</div>');
+}]);
+})();
+
+(function(module) {
+try {
+  module = angular.module('ngLayoutPage');
+} catch (e) {
+  module = angular.module('ngLayoutPage', []);
+}
+module.run(['$templateCache', function($templateCache) {
+  $templateCache.put('pane-header/pane-header.html',
+    '<a href="#" class="pane-header-close" ng-if="vm.showClose" ng-click="vm.close()">\n' +
+    '    <span class="pane-header-closeIcon fa fa-close"></i>\n' +
+    '</a>\n' +
+    '<div class="pane-header-actions" ng-transclude></div>\n' +
+    '<div class="pane-header-titles">\n' +
+    '    <div class="pane-header-title">\n' +
+    '        <span class="pane-header-titleText">{{vm.title}}</span>\n' +
+    '    </div>\n' +
+    '    <div class="pane-header-subtitle">\n' +
+    '        <span class="pane-header-subtitleText" ng-if="vm.subtitle">{{vm.subtitle}}</span>\n' +
     '    </div>\n' +
     '</div>');
 }]);
