210330d1132903da46f7dcb424cfaae854978673
John Hampton <john_j_hampton@yahoo.com>
2018-04-23 17:37:11 -0400

    Rebuild of previous release

---
+++ dist/vops-layout.d.ts
@@ -0,0 +1,64 @@
+declare module LayoutPageModule {
+}
+declare module LayoutPageModule {
+}
+declare module LayoutPageModule {
+}
+declare module LayoutPageModule {
+}
+declare module LayoutPageModule {
+    interface IPageOverlay {
+    }
+    interface ILayoutPageController extends IPageOverlay {
+        showNav(): any;
+        hideNav(): any;
+        toggleNav(): any;
+        showOverlay(overlay: IPageOverlay): any;
+        hideOverlay(overlay: IPageOverlay): any;
+    }
+}
+declare module LayoutPageModule {
+}
+declare module LayoutPageModule {
+}
+declare module LayoutPageModule {
+    interface IPageController {
+        addControl($element: any): any;
+        ensureOnTop($element: any): any;
+    }
+}
+declare module LayoutPageModule {
+}
+declare module LayoutPageModule {
+}
+declare module LayoutPageModule {
+}
+declare module LayoutPageModule {
+    interface IPageSliderController extends IPageOverlay {
+        isVisible: any;
+        withOverlay: any;
+        close(): any;
+    }
+}
+declare module LayoutPageModule {
+}
+declare module LayoutPageModule {
+}
+declare module LayoutPageModule {
+}
+declare module LayoutPageModule {
+    interface ITabController {
+        title: string;
+        name: string;
+        icon: string;
+    }
+}
+declare module LayoutPageModule {
+    interface ITabsController {
+        addTab(tab: ITabController): any;
+        selectTabByName(name: string): any;
+        selectTabByIndex(idx: number): any;
+        selectNextTab(): any;
+        selectPreviousTab(): any;
+    }
+}
